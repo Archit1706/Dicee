36b4700360cfee793160d5c86b3e6b090a627b61
Archit Rathod <75872913+Archit1706@users.noreply.github.com>
2022-06-16 01:34:37 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,12 @@
+randomNumber1 = Math.floor(6 * Math.random()) + 1;
+document.querySelector(".dice img").src = "images/dice" + randomNumber1 + ".png"
+randomNumber2 = Math.floor(6 * Math.random()) + 1;
+document.querySelectorAll(".dice img")[1].src = "images/dice" + randomNumber2 + ".png"
+
+if (randomNumber1 == randomNumber2) {
+    document.querySelector("h1").innerHTML = "Draw!"
+} else if (randomNumber1 > randomNumber2) {
+    document.querySelector("h1").innerHTML = "🚩Player 1 Wins!"
+} else {
+    document.querySelector("h1").innerHTML = "Player 2 Wins!🚩"
+}
